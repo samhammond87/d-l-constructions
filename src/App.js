fc574f881b37565ff410f963a85b2070587cd094
Trac3yTran <tran.tracey90@gmail.com>
2021-01-29 00:12:57 +1100

    contact form done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages";
-import SigninPage from "./pages/signin";
-import Test from './components/SignIn/sign-in'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages';
+import SigninPage from './pages/signin';
+import Test from './components/SignIn/sign-in';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/signin" component={SigninPage} exact />
-        <Route path="/test" component={Test} exact />
+        <Route path='/' component={Home} exact />
+        <Route path='/signin' component={SigninPage} exact />
+        <Route path='/test' component={Test} exact />
       </Switch>
     </Router>
   );

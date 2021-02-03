f0e500378e7472867ea99349fb13f18995591a99
yee-codes <shaoyee.ng@gmail.com>
2021-02-03 15:48:35 +1100

    aws s3 set up to store static assets

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Icon1 from "../../images/svg-4.png";
-import Icon2 from "../../images/svg-5.png";
-import Icon3 from "../../images/svg-6.png";
+// import Icon1 from "../../images/svg-4.png";
+// import Icon2 from "../../images/svg-5.png";
+// import Icon3 from "../../images/svg-6.png";
 import {
   ServicesContainer,
   ServicesH1,
@@ -18,7 +18,8 @@ const Services = () => {
       <ServicesH1>Why us?</ServicesH1>
       <ServicesWrapper>
         <ServicesCard>
-          <ServicesIcon src={Icon1} />
+          {/* <ServicesIcon src={Icon1} /> */}
+          <ServicesIcon src="https://full-stack-app.s3-ap-southeast-2.amazonaws.com/images/svg-4.png" />
           <ServicesH2>Fast & Effective Services</ServicesH2>
           <ServicesP>
             We have the experience & resources to make the project run smoothly.
@@ -26,7 +27,8 @@ const Services = () => {
           </ServicesP>
         </ServicesCard>
         <ServicesCard>
-          <ServicesIcon src={Icon2} />
+          {/* <ServicesIcon src={Icon2} /> */}
+          <ServicesIcon src="https://full-stack-app.s3-ap-southeast-2.amazonaws.com/images/svg-5.png" />
           <ServicesH2>Value For Money</ServicesH2>
           <ServicesP>
             Competitive price with excellent customer service (quick return and
@@ -34,7 +36,8 @@ const Services = () => {
           </ServicesP>
         </ServicesCard>
         <ServicesCard>
-          <ServicesIcon src={Icon3} />
+          {/* <ServicesIcon src={Icon3} /> */}
+          <ServicesIcon src="https://full-stack-app.s3-ap-southeast-2.amazonaws.com/images/svg-6.png" />
           <ServicesH2>Customer Satisfaction</ServicesH2>
           <ServicesP>
             We will do everything possible, use the necessary resources to solve

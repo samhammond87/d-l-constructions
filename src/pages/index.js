bf111a6a7a42d723f135d271b7424ba9bdb10453
Trac3yTran <tran.tracey90@gmail.com>
2021-02-03 20:19:12 +1100

    contact form restyling and added simple alert message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,17 @@
-import React, { useState } from "react";
-import HeroSection from "../components/HeroSection";
-import InfoSection from "../components/InfoSection";
-import Navbar from "../components/Navbar";
-import TestButton from "../components/TestButton";
-import Footer from "../components/Footer";
-import Services from "../components/Services";
-import Sidebar from "../components/Sidebar";
+import React, { useState } from 'react';
+import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import Navbar from '../components/Navbar';
+import TestButton from '../components/TestButton';
+import Footer from '../components/Footer';
+import Services from '../components/Services';
+import Sidebar from '../components/Sidebar';
 import {
   homeObjOne,
   homeObjTwo,
   homeObjThree,
-} from "../components/InfoSection/Data";
-
-import ContactUs from "../components/Contact/ContactForm";
+} from '../components/InfoSection/Data';
+import ContactUs from '../components/Contact';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);

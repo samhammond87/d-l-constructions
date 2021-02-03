cfed312edc3bffc351479695628639104c39a21e
yee-codes <shaoyee.ng@gmail.com>
2021-02-03 22:59:48 +1100

    add styling to show page

--- src/components/Portal/NewTimesheet.js
+++ src/components/Portal/NewTimesheet.js
@@ -10,14 +10,14 @@ import { useGlobalState } from "./utils/stateContext";
 import "./NewTimesheetElements.css";
 
 export default function NewTimesheet() {
-	const initialFormState = {
-		name: '',
-		date: '',
-		start_time: '',
-		end_time: '',
-		total_hours: '',
-		comments: ''
-
+  const initialFormState = {
+    name: "",
+    date: "",
+    start_time: "",
+    end_time: "",
+    total_hours: "",
+    comments: "",
+  };
 
   const [formState, setFormState] = useState(initialFormState);
   let history = useHistory();
--- src/components/Portal/PortalNav.js
+++ src/components/Portal/PortalNav.js
@@ -1,43 +1,42 @@
-import React from 'react'
-import {useHistory} from 'react-router-dom'
-import {Button, Panel, Span} from './Styled'
-import {useGlobalState} from './utils/stateContext'
-import {signOut} from './services/authServices'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Button, Panel, Span } from "./Styled";
+import { useGlobalState } from "./utils/stateContext";
+import { signOut } from "./services/authServices";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 export default function PortalNav() {
-	let history = useHistory()
+  let history = useHistory();
 
-	const {store,dispatch} = useGlobalState()
-	const {loggedInUser} = store
+  const { store, dispatch } = useGlobalState();
+  const { loggedInUser } = store;
 
+  function handleSignOut(event) {
+    event.preventDefault();
+    signOut(loggedInUser).then(() => {
+      dispatch({ type: "setLoggedInUser", data: null });
+      dispatch({ type: "setToken", data: null });
+    });
+  }
 
-	function handleSignOut(event) {
-		event.preventDefault()
-		signOut(loggedInUser)
-		.then(() => {
-			dispatch({type: 'setLoggedInUser', data: null})
-			dispatch({type: 'setToken', data: null})
-		})
-	}
-
-	return (
-		<Panel>
-		
-			<Button onClick={() => history.push('/portal')}>Home</Button>
-			{loggedInUser ?
-				<>
-				<Button onClick={() => history.push('/portal/new') }>Add Timesheet</Button>	
-				<Button onClick={handleSignOut}>Sign Out</Button>	
-				<Span>Welcome {loggedInUser}</Span>
-				</>
-			:
-				<>
-				<Button onClick={() => history.push('/register')}>Register</Button>
-				<Button onClick={() => history.push('/sign_in')}>Sign In</Button>
-				</>
-			}
-		</Panel>
-	)
+  return (
+    <Panel>
+      <Button onClick={() => history.push("/portal")}>Home</Button>
+      {loggedInUser ? (
+        <>
+          <Button onClick={() => history.push("/portal/new")}>
+            Add Timesheet
+          </Button>
+          <Button onClick={handleSignOut}>Sign Out</Button>
+          <Span>Welcome {loggedInUser}</Span>
+        </>
+      ) : (
+        <>
+          <Button onClick={() => history.push("/register")}>Register</Button>
+          <Button onClick={() => history.push("/sign_in")}>Sign In</Button>
+        </>
+      )}
+    </Panel>
+  );
 }

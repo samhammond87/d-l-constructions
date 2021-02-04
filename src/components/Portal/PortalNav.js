cf1cf236534814b60df5ab009690d54628209f15
yee-codes <shaoyee.ng@gmail.com>
2021-02-04 12:31:15 +1100

    portal navbar styling

--- src/components/Portal/PortalNav.js
+++ src/components/Portal/PortalNav.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Button, Panel, Span } from "./Styled";
+import { Button } from "./Styled";
 import { useGlobalState } from "./utils/stateContext";
 import { signOut } from "./services/authServices";
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+// import Nav from "react-bootstrap/Nav";
+import "./PortalNav.css";
 
 export default function PortalNav() {
   let history = useHistory();
@@ -20,37 +21,74 @@ export default function PortalNav() {
     });
   }
 
+  // function handleSignOut(event) {
+  // 	event.preventDefault()
+  // 	signOut(loggedInUser)
+  // 	.then(() => {
+  // 		dispatch({type: 'setLoggedInUser', data: null})
+  // 		dispatch({type: 'setToken', data: null})
+  // 	})
+  // }
 
-	function handleSignOut(event) {
-		event.preventDefault()
-		signOut(loggedInUser)
-		.then(() => {
-			dispatch({type: 'setLoggedInUser', data: null})
-			dispatch({type: 'setToken', data: null})
-		})
-	}
+  return (
+    <>
+      <div className="nav-container">
+        <nav className="navbar">
+          <h1 id="navbar-logo">D&L Constructions</h1>
+          <div className="menu-toggle" id="mobile-menu">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+          <ul className="nav-menu">
+            <li>
+              <a href="#" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link">
+                Register
+              </a>
+            </li>
+            <li>
+              <a href="#" className="nav-link nav-links-btn">
+                Sign In
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>{loggedInUser}'s Timesheets</Navbar.Brand>
+        <Navbar.Brand>
+          <Button onClick={() => history.push("/portal")}>Home</Button>
+        </Navbar.Brand>
+        {loggedInUser ? (
+          <>
+            <Navbar.Brand>
+              <Button onClick={() => history.push("/portal/new")}>
+                Add Timesheet
+              </Button>
+            </Navbar.Brand>
 
-	return (
-		<Navbar bg="dark" variant="dark">
-			<Navbar.Brand>{loggedInUser}'s Timesheets</Navbar.Brand>
-			<Navbar.Brand><Button onClick={() => history.push('/portal')}>Home</Button></Navbar.Brand>
-			{loggedInUser ?
-				<>
-				<Navbar.Brand><Button onClick={() => history.push('/portal/new') }>Add Timesheet</Button></Navbar.Brand>
-
-				<Navbar.Brand><Button onClick={handleSignOut}>Sign Out</Button>	</Navbar.Brand>	
-				</>
-			:
-				<>
-				<Navbar.Brand>
-				<Button onClick={() => history.push('/register')}>Register</Button>
-				</Navbar.Brand>
-				<Navbar.Brand>
-				<Button onClick={() => history.push('/sign_in')}>Sign In</Button>
-				</Navbar.Brand>
-				</>
-			}
-		</Navbar>
-	)
-
+            <Navbar.Brand>
+              <Button onClick={handleSignOut}>Sign Out</Button>{" "}
+            </Navbar.Brand>
+          </>
+        ) : (
+          <>
+            <Navbar.Brand>
+              <Button onClick={() => history.push("/register")}>
+                Register
+              </Button>
+            </Navbar.Brand>
+            <Navbar.Brand>
+              <Button onClick={() => history.push("/sign_in")}>Sign In</Button>
+            </Navbar.Brand>
+          </>
+        )}
+      </Navbar>
+    </>
+  );
 }

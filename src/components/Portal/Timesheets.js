67658233750f0e9fac6d7a80c764da83daf9ce88
yee-codes <shaoyee.ng@gmail.com>
2021-02-05 17:58:52 +1100

    add search by date function

--- src/components/Portal/Timesheets.js
+++ src/components/Portal/Timesheets.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 // import styled from 'styled-components'
 // import Timesheet from './Timesheet'
 import { useGlobalState } from "../../utils/stateContext";
-import { Card, Table, Container, Row, Col } from "react-bootstrap";
+// import { Card, Table, Container, Row, Col } from "react-bootstrap";
 
 export default function Timesheets() {
-  const [searchName, setSearchName] = useState("");
+  const [searchEntry, setSearchEntry] = useState("");
 
   const { store } = useGlobalState();
   const { timesheets } = store;
@@ -14,9 +14,11 @@ export default function Timesheets() {
 
   const list = timesheets
     .filter((entry) => {
-      if (searchName === "") {
+      if (searchEntry === "") {
         return entry;
-      } else if (entry.name.toLowerCase().includes(searchName.toLowerCase())) {
+      } else if (entry.name.toLowerCase().includes(searchEntry.toLowerCase())) {
+        return entry;
+      } else if (entry.date.toLowerCase().includes(searchEntry.toLowerCase())) {
         return entry;
       }
     })
@@ -46,14 +48,14 @@ export default function Timesheets() {
     <>
       <div className="searchContainer" style={{ textAlign: "center" }}>
         <label className="searchLabel" style={{ marginRight: "20px" }}>
-          Search by name:
+          Enter name or date:
         </label>
         <input
           type="text"
-          placeholder="Enter search..."
+          placeholder="Search......"
           className="searchField"
           onChange={(event) => {
-            setSearchName(event.target.value);
+            setSearchEntry(event.target.value);
           }}
         />
       </div>
@@ -71,31 +73,6 @@ export default function Timesheets() {
         </thead>
         <tbody>{list}</tbody>
       </table>
-
-      <Container fluid>
-        <Row>
-          <Col md="12">
-            <Card className="striped-tabled-with-hover">
-              <Card.Body className="table-full-width table-responsive px-0">
-                <Table className="table-hover table-striped">
-                  <thead>
-                    <tr>
-                      <th className="border-0">Name</th>
-                      <th className="border-0">Date</th>
-                      <th className="border-0">Start Time</th>
-                      <th className="border-0">End Time</th>
-                      <th className="border-0">Total Hours</th>
-                      <th className="border-0">Update?</th>
-                      <th className="border-0">Delete?</th>
-                    </tr>
-                  </thead>
-                  <tbody>{list}</tbody>
-                </Table>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
     </>
   );
 }

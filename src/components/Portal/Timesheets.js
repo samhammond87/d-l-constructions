fe5480db762256d3e14231c8f3251c0836d9694b
yee-codes <shaoyee.ng@gmail.com>
2021-02-05 17:21:49 +1100

    add search function by name

--- src/components/Portal/Timesheets.js
+++ src/components/Portal/Timesheets.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 // import styled from 'styled-components'
 // import Timesheet from './Timesheet'
@@ -6,34 +6,57 @@ import { useGlobalState } from "../../utils/stateContext";
 import { Card, Table, Container, Row, Col } from "react-bootstrap";
 
 export default function Timesheets() {
+  const [searchName, setSearchName] = useState("");
+
   const { store } = useGlobalState();
   const { timesheets } = store;
   if (!timesheets) return null;
 
-  const list = timesheets.map((timesheet, index) => {
-    return (
-      <tr className="active-row">
-        <td key={index}>
-          <Link
-            style={{ color: "#009879" }}
-            key={timesheet.id}
-            to={`/portal/${timesheet.id}`}
-          >
-            {timesheet.name}
-          </Link>
-        </td>
-        <td>{timesheet.date}</td>
-        <td>{timesheet.start_time}</td>
-        <td>{timesheet.end_time}</td>
-        <td>{timesheet.total_hours}</td>
-        <td></td>
-        <td></td>
-      </tr>
-    );
-  });
+  const list = timesheets
+    .filter((entry) => {
+      if (searchName === "") {
+        return entry;
+      } else if (entry.name.toLowerCase().includes(searchName.toLowerCase())) {
+        return entry;
+      }
+    })
+    .map((timesheet, index) => {
+      return (
+        <tr className="active-row">
+          <td key={index}>
+            <Link
+              style={{ color: "#009879" }}
+              key={timesheet.id}
+              to={`/portal/${timesheet.id}`}
+            >
+              {timesheet.name}
+            </Link>
+          </td>
+          <td>{timesheet.date}</td>
+          <td>{timesheet.start_time}</td>
+          <td>{timesheet.end_time}</td>
+          <td>{timesheet.total_hours}</td>
+          <td></td>
+          <td></td>
+        </tr>
+      );
+    });
 
   return (
     <>
+      <div className="searchContainer" style={{ textAlign: "center" }}>
+        <label className="searchLabel" style={{ marginRight: "20px" }}>
+          Search by name:
+        </label>
+        <input
+          type="text"
+          placeholder="Enter search..."
+          className="searchField"
+          onChange={(event) => {
+            setSearchName(event.target.value);
+          }}
+        />
+      </div>
       <table className="styled-table">
         <thead>
           <tr>

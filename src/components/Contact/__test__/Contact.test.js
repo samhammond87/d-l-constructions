903c77c327cab008eaad59b18303a000b24f3196
yee-codes <shaoyee.ng@gmail.com>
2021-02-06 12:03:32 +1100

    code comment contact form component

--- src/components/Contact/__test__/Contact.test.js
+++ src/components/Contact/__test__/Contact.test.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// test suite for contact form component
+
+// test if the entire contact form renders properly
 it("contact form renders without error", () => {
   const div = document.createElement("div");
   ReactDOM.render(<div></div>, div);
 });
 
+// test if contact form labels render properly
 it("all input labels renders without error", () => {
   const label = document.createElement("label");
   ReactDOM.render(<label></label>, label);
 });
 
+// test if contact form input fields render properly
 it("contact form submit button renders", () => {
   const input = document.createElement("input");
   ReactDOM.render(<input></input>, input);
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import emailjs from "emailjs-com";
 import {
   ContactFormContent,
   ContactForm,
@@ -7,19 +8,20 @@ import {
   ContactFormInput,
   ContactFormTextMessageInput,
   ContactFormButton,
-} from './ContactFormElements';
-import emailjs from 'emailjs-com';
+} from "./ContactFormElements";
 
 export default function ContactUs() {
   function sendEmail(e) {
+    // prevents browser from refreshing when the button is clicked
     e.preventDefault();
 
+    // emailjs configuration
     emailjs
       .sendForm(
-        'service_4aatm2p',
-        'template_3ww3nz1',
+        "service_4aatm2p",
+        "template_3ww3nz1",
         e.target,
-        'user_AI5Txgn8xAtQwnf1LvUBq'
+        "user_AI5Txgn8xAtQwnf1LvUBq"
       )
       .then(
         (result) => {
@@ -32,35 +34,36 @@ export default function ContactUs() {
     e.target.reset();
   }
 
+  // prompt to inform user that message has been sent successfully
   function emailAlert() {
     alert("Thank you for getting in touch! We'll be in contact soon");
   }
 
   return (
     <>
-      <ContactFormContent id='contact'>
+      <ContactFormContent id="contact">
         <ContactForm onSubmit={sendEmail}>
           <ContactFormH1>Contact Us</ContactFormH1>
-          <ContactFormLabel htmlFor='for'>Name</ContactFormLabel>
-          <ContactFormInput type='text' className='form-control' name='name' />
-          <ContactFormLabel htmlFor='for'>Email</ContactFormLabel>
+          <ContactFormLabel htmlFor="for">Name</ContactFormLabel>
+          <ContactFormInput type="text" className="form-control" name="name" />
+          <ContactFormLabel htmlFor="for">Email</ContactFormLabel>
           <ContactFormInput
-            type='email'
-            className='form-control'
-            name='email'
+            type="email"
+            className="form-control"
+            name="email"
           />
-          <ContactFormLabel htmlFor='for'>Subject</ContactFormLabel>
+          <ContactFormLabel htmlFor="for">Subject</ContactFormLabel>
           <ContactFormInput
-            type='text'
-            className='form-control'
-            name='subject'
+            type="text"
+            className="form-control"
+            name="subject"
           />
-          <ContactFormLabel htmlFor='for'>Message</ContactFormLabel>
+          <ContactFormLabel htmlFor="for">Message</ContactFormLabel>
           <ContactFormTextMessageInput
-            className='form-control'
-            name='message'
+            className="form-control"
+            name="message"
           />
-          <ContactFormButton type='submit' onClick={emailAlert}>
+          <ContactFormButton type="submit" onClick={emailAlert}>
             Send Message
           </ContactFormButton>
           {/* <Text>Forgot password?</Text> */}
